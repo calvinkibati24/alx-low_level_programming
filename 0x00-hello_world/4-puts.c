b308201dc889b980dbffeeffe39e9276b99576ee
Calvin Kibati <calvinn.invests@gmail.com>
2023-03-16 03:40:47 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - programs entry point that prints a line with puts functtion
+ *Return: 0 (success)
+ */
+int main(void)
+}
+puts("\""Programming is like building a multilingual puzzle");
+return (0);
+}
