04ecfa3ed733d1541cc051698ff79827d212c92a
Calvin Kibati <calvinn.invests@gmail.com>
2023-03-17 04:09:17 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - prints all single digit numbers of base ten starting from zero
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int d;
+for (d = '0'; d <= '9'; d++)
+{
+putchar(d);
+putchar('\n');
+}
+return (0);
+}
