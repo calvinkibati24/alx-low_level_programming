6b9eccd97683e80ec3297f0d02c70cc6a819a4c0
Calvin Kibati <calvinn.invests@gmail.com>
2023-03-17 02:14:38 -0700

    task 0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - print whether the number stored is positive or negative
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int n;
+if (n > 0)
+{
+printf("%d is positive\n", n);
+}
+else if (n == 0)
+{
+printf("%d is zero\n", n);
+}
+else
+printf("%d is negative\n", n);
+}
+return (0);
+}
