c035a05aea2dac7b6cb5bfc29a19f9b89f7f911c
Calvin Kibati <calvinn.invests@gmail.com>
2023-03-17 03:11:13 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio>
+/**
+ * main - prints the alphabet in lowercase
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+char lcase, e, q;
+e = 'e';
+q = 'q';
+for (lcase = 'a'; lcase <= 'z'; lcase++)
+{
+if (lcase != 'e' && lcase != 'q')
+{
+putchar(lcase);
+}
+putchar('\n');
+return (0);
+}}
