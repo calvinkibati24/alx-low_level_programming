6b46c63ef9a7d83898656e8421a4c9629fd8b3ef
Calvin Kibati <calvinn.invests@gmail.com>
2023-03-21 04:17:32 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - program that prints _putchar
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+char print = "_putchar";
+_putchar(print);
+_putchar('\n');
+return (0);
+}
