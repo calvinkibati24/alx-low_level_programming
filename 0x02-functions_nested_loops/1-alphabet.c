9d7b0763966ddac785ddb7a66149d9c6ff4ea99c
Calvin Kibati <calvinn.invests@gmail.com>
2023-03-21 04:57:03 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
  * print_alphabet - prints the alphabet, in lowercase
- * Return: 0 (Success)
  */
 void print_alphabet(void)
 {
@@ -9,6 +8,4 @@ char letter;
 for (letter = 'a'; letter <= 'z'; letter++)
 _putchar(letter);
 _putchar('\n');
-return (0);
 }
-
