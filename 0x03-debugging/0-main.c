ffceec9c5649a10b725d4c73ab012c36341d4d34
Calvin Kibati <calvinn.invests@gmail.com>
2023-03-22 00:56:05 -0700

    task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* main - file must test that the function positive_or_negative()
+* Return: 0 (Successful)
+*/
+int main(void)
+{
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
+}
