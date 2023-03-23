33c77f919831c70c9cd3fbb0492ca22dab03035e
Calvin Kibati <calvinn.invests@gmail.com>
2023-03-23 04:26:00 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - function that multiplies two integers
+ * @a: first int
+ * @b: second int
+ * Return: addition of the multiplication
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
