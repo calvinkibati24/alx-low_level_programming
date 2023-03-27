62b3de015527dde6548f27d0ab6a912205a3b6f1
Calvin Kibati <calvinn.invests@gmail.com>
2023-03-27 07:48:03 -0700

    task 11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - program that generates random valid
+ * main - a program that generates random valid
  * passwords for the program 101-crackme
  *
  * Return: Always 0 (Success)
