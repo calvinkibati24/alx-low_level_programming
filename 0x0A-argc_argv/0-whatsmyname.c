200be6757bda4efaf1bdb1030e2703b8e956a2ac
Calvin Kibati <calvinn.invests@gmail.com>
2023-04-10 00:26:29 -0700

    task 0 correction

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - program that prints its name, followed by a new line
+ * main - program that prints its name,followed by a new line
  * @argc: number of arguments
  * @argv: array of arguments
  * Return: Always 0 (Success)
