9388eda5de7dcbfcc35ec32d65e46a3c50249078
Calvin Kibati <calvinn.invests@gmail.com>
2023-04-13 02:28:16 -0700

    task 1 correction

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,7 +14,8 @@ unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 while (s1 && s1[len1])
 len1++;
 while (s2 && s2[len2])
-len2++;if (n < len2)
+len2++;
+if (n < len2)
 s = malloc(sizeof(char) * (len1 + n + 1));
 else
 s = malloc(sizeof(char) * (len1 + len2 + 1));
