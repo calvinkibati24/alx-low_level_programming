18542c83a77ef793a8f57cc2d62e664eeec0b4d5
Calvin Kibati <calvinn.invests@gmail.com>
2023-04-17 11:39:52 -0700

    task 2 and 5

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,7 +11,7 @@ if (d == NULL)
 return;
 if (d->name == NULL)
 d->name = "(nil)";
-if (d->owner =! NULL)
+if (d->owner == NULL)
 d->owner = "(nil)";
 printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
